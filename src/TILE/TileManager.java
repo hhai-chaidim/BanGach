3598fb30e362812d6f4939289bd4670da129d415
Nguyen Hoang Hai <hai18042006@gmail.com>
2025-09-29 17:06:27 +0700

    ahieu

--- src/TILE/TileManager.java
+++ src/TILE/TileManager.java
@@ -21,30 +21,15 @@ public class TileManager {
     public void getTileImage() {
         try{
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Background/DungeonTileSet.png")));
+            tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Background/ahieu.png")));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void draw(Graphics2D g2) {
-        int col = 0;
-        int row = 0;
-        int x = 0;
-        int y = 0;
-
-        while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
-            col++;
-            x += gp.tileSize;
-
-            if(col == gp.maxScreenCol) {
-                col = 0;
-                x = 0;
-                row++;
-                y += gp.tileSize;
-            }
-        }
+public void draw(Graphics2D g2) {
+        // Draw the image once, stretched to fit the entire screen
+        g2.drawImage(tile[0].image, 0, 0, gp.screenWidth, gp.screenHeight, null);
     }
 }
